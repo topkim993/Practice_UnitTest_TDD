def6ac479297bf68f11db5fce69302bacf2f93d2
odongnamu <odongnamu@odongnamuui-MacBookPro.local>
2022-02-14 18:57:58 +0900

    first commit

---
+++ StrangeCalculatorTests/StrangeCalculatorTests.swift
@@ -0,0 +1,32 @@
+//
+// © 2022. yagom academy all rights reserved
+// This tutorial is produced by Yagom Academy and is prohibited from redistributing or reproducing.
+//
+
+
+import XCTest
+
+class StrangeCalculatorTests: XCTestCase {
+    // TODO: TDD로 정수 타입을 파라미터로 받아, 그 절대값이 홀수이면 부호를 바꾸어서 반환하는 메서드 구현하기
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ UnitTestSample/Practice/StrangeCalculator.swift
@@ -0,0 +1,10 @@
+//
+// © 2022. yagom academy all rights reserved
+// This tutorial is produced by Yagom Academy and is prohibited from redistributing or reproducing.
+//
+
+import Foundation
+
+struct StrangeCalculator {
+    // TODO: TDD로 정수 타입을 파라미터로 받아, 그 절대값이 홀수이면 부호를 바꾸어서 반환하는 메서드 구현하기
+}
